977fd4af63dc59f465be3da6b7965a39e144fb47
typingmistake <tjsqls2119@cau.ac.kr>
2024-09-21 17:25:36 +0900

    feat : 로그인,로그아웃 scss 작성, 로그인 여부 확인 미들웨어 추가

--- src/middleWare.js
+++ src/middleWare.js
@@ -6,3 +6,16 @@ export const errorHandlingMiddleware = (error, req, res, next) => {
             message: error.message || '서버 내부에서 오류가 발생했습니다.'
         });
 }
+
+// 로그인 여부 확인 미들웨어
+export const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.status(401).json({
+        ok: false,
+        message: '로그인이 필요합니다.',
+        redirectUrl: '/login'
+    });
+}
